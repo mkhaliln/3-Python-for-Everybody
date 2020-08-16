21d30542f8cdd158df7d702d0cc466af46ed2fea
mkhaliln <mkhalil@protonmail.com>
2020-08-16 16:27:56 +0530

    Exercise 3 & 5 Solutions

---
+++ l2-ex3.py
@@ -0,0 +1,10 @@
+# Asking user for number of hours
+hours = int(input("Enter Hours: "))
+
+# Asking user for rate
+rate = int(input("Enter Rate: "))
+
+#Calculating the total pay
+pay = hours*rate
+
+print("Pay: ", pay)
---
+++ l2-ex5.py
@@ -0,0 +1,8 @@
+
+# Asking to enter celcius and changing the type to float
+celc = float(input("Enter a Celcius temprature: "))
+
+#Changing the celcius to fahrenheit according to formula
+fahre = (celc * 9/5) + 32
+
+print(celc, " Celcius equals to ", fahre, " Fahrenheits")
